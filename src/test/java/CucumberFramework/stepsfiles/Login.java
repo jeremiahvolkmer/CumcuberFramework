305ceb3784bea7d71163abe70019bbe9351e14e7
Jeremiah Volkmer <jvolkmer32@gmail.com>
2021-03-11 19:43:57 -0800

    finished writing test cases

--- src/test/java/CucumberFramework/stepsfiles/Login.java
+++ src/test/java/CucumberFramework/stepsfiles/Login.java
@@ -2,7 +2,9 @@ package CucumberFramework.stepsfiles;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import cucumber.api.java.After;
@@ -11,6 +13,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class Login {
 	WebDriver driver; 
@@ -30,35 +33,41 @@ public class Login {
 	public void tearDown()
 	{
 		this.driver.manage().deleteAllCookies();
-		this.driver.quit();
-		this.driver = null; 
+	//	this.driver.quit();
+	//	this.driver = null; 
 	}
 	@Given("^User navigates to stackoverflow website$")
 	public void user_navigates_to_stackoverflow_website() throws Throwable {
-	    System.out.println("Method 1");
+	    driver.get("https://stackoverflow.com/");
 	}
 
 	@And("^User clicks on the login button on homepage$")
 	public void user_clickes_on_the_login_button_on_homepage() throws Throwable {
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[text()='Log in']")).click();
 	}
 
 	@And("^User enters a valid username$")
 	public void user_enters_a_valid_username() throws Throwable {
-		System.out.println("Method 3");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("autotestudemy@mail.com");
 	}
 
 	@And("^User enters a valid password$")
 	public void user_enters_a_valide_password() throws Throwable {
-		System.out.println("Method 4");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("Password321!");
 	}
 
 	@When("^User clicks on the login button$")
 	public void user_clicks_on_the_login_button() throws Throwable {
-		System.out.println("Method 5");
+		driver.findElement(By.xpath("//*[@id=\"submit-button\"]")).click(); 
+		
 	}
 
 	@Then("^User should be taken to the successful login page$")
 	public void user_should_be_taken_to_the_successful_login_page() throws Throwable {
-		System.out.println("Method 6");
+		Thread.sleep(3000);
+		WebElement askQuestionButton = driver.findElement(By.xpath("//a[contains(text(), 'Ask Question')]"));
+		Assert.assertEquals(true, askQuestionButton.isDisplayed());
 	}
 }
